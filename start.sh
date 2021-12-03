9c9ffba07484d1752ec9f09fa94068a9dd70adca
Damon92 <874467785@qq.com>
2021-12-03 09:43:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ sleep 2
 /root/vpnserver/vpnserver start
 sleep 3
 #GZQcloud互联
-#ifconfig tap_gzqcloud 172.16.255.2 netmask 255.255.255.252
+ifconfig tap_gzqcloud 172.16.255.2 netmask 255.255.255.252
